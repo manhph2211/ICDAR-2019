4e478db8c132864a57ebed8e588b3c20f6c73dd1
manhph2211 <maxph22112000@gmail.com>
2021-02-09 11:17:10 +0700

    sad

--- task1/model.py
+++ task1/model.py
@@ -10,16 +10,4 @@ from sklearn.metrics import precision_score, f1_score, recall_score
 
 
 
-class RNN(nn.Module):
-    def __init__(self, input_size, output_size, hidden_dim, n_layers):
-        super(RNN, self).__init__()
-        self.rnn = nn.RNN(input_size=input_size, hidden_size=hidden_dim, num_layers=n_layers, nonlinearity='tanh')
-        self.linear1 = nn.Linear(in_features=hidden_dim, out_features=output_size)
-        self.classifier = nn.Softmax()
-
-    def forward(self, X):
-        out,hidden = self.rnn(X)
-        out = out[:, -1, :]
-        out = self.linear1(out)
-        return out
 
--- task1/utils.py
+++ task1/utils.py
@@ -2,6 +2,9 @@ import glob
 import os
 import re
 import json
+import cv2
+import matplotlib.pyplot as plt 
+
 # image_path=glob.glob(os.path.join(data_train_path,'*.jpg'))
 # txt_path=glob.glob(os.path.join(data_train_path,'*.txt'))
 
@@ -26,3 +29,30 @@ def saveJsonFile(data):
 
 # data=getData()
 # saveJsonFile(data)
+
+
+# test image 
+# data=readJsonFile('./data_task1_train.json')
+'''
+def showRetangleFromFile(k,v):
+	img=cv2.imread(k)
+	with open(v,'r') as f:
+		for line in f:
+			bx=line.split(',')
+			x=int(bx[0])
+			y=int(bx[1])
+			a=int(bx[4])
+			b=int(bx[5])
+			cv2.rectangle(img, (x, y), (a, b), (0, 255,0), 2)
+	plt.imshow(img)
+	plt.show()
+			
+'''
+
+
+def test(img,corList):  # corList [[x1_1, y1_1,x3_1,y3_1],...]
+	for obj in corList: 
+		cv2.rectangle(img, (obj[0], obj[1]), (obj[4], obj[5]), (0, 255,0), 2 )
+	plt.imshow(img)
+	plt.show()
+
