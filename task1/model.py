f05148d36de1fb47fb73e5a9b5d5298dfd757328
manhph2211 <maxph22112000@gmail.com>
2021-02-20 14:14:25 +0700

    update

--- task1/model.py
+++ task1/model.py
@@ -1,16 +1,9 @@
 import torch
-import numpy as np
 from torch import nn
-from torch.utils.data import DataLoader
-from torch.optim import Adam
-from sklearn.model_selection import train_test_split
-from tqdm import tqdm
-import matplotlib.pyplot as plt
-from sklearn.metrics import precision_score, f1_score, recall_score
 from l2_norm import L2Norm
 from default_box import DefBox
 from torch.autograd import Function
-
+import torch.nn.functional as F
 
 def create_vgg():
     layers = []
