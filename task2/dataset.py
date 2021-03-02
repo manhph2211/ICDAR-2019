5c06192e2d2648fa7872d532d6d782ba934d71de
manhph2211 <maxph22112000@gmail.com>
2021-03-02 23:19:33 +0700

    update

--- task2/dataset.py
+++ task2/dataset.py
@@ -1,11 +1,7 @@
-# tough one
-
-
 import torch
 import cv2
 from torchvision import transforms
 from torch.utils.data import Dataset
-
 
 class my_dataset(Dataset):
   
@@ -15,10 +11,13 @@ class my_dataset(Dataset):
    
   def __getitem__(self,idx):
     img=cv2.imread(self.img_paths[idx])
-    #img=cv2.resize(img,(100,32)) 
+    img=cv2.resize(img,(100,32)) 
     img=torch.tensor(img, dtype=torch.float32)
     img=img.permute(2, 0, 1)
-    txt=get_text(self.txt_paths[idx])
+    img_transform=transforms.Compose([transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+    img=img_transform(img)
+    txt=self.txt_paths[idx]
+    txt=read_text_file(txt)
     txt=encode(txt)
     txt=torch.IntTensor(txt)
     return img,txt
@@ -27,50 +26,12 @@ class my_dataset(Dataset):
     return len(self.img_paths)
 
 
-# def my_collate_fn(batch):
-#     imgs, labels = zip(*batch)
-#     #imgs = torch.stack(imgs, dim=0)
-#     imgs = torch.cat([t.unsqueeze(0) for t in imgs], 0) 
-#     return imgs, labels
-
-class resizeNormalize(object):
-
-    def __init__(self, size):
-        self.size = size
-
-    def __call__(self, img):
-        img = cv2.resize(img,(self.size,self.size))
-        img_transform=transforms.Compose([transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-        img=transform(img)
-        return img
-
-
-class alignCollate(object):
-
-    def __init__(self, imgH=32, imgW=100, keep_ratio=False, min_ratio=1):
-        self.imgH = imgH
-        self.imgW = imgW
-        self.keep_ratio = keep_ratio
-        self.min_ratio = min_ratio
-
-    def __call__(self, batch):
-        images, labels = zip(*batch)
-
-        imgH = self.imgH
-        imgW = self.imgW
-        if self.keep_ratio:
-            ratios = []
-            for image in images:
-                w, h = image.size
-                ratios.append(w / float(h))
-            ratios.sort()
-            max_ratio = ratios[-1]
-            imgW = int(np.floor(max_ratio * imgH))
-            imgW = max(imgH * self.min_ratio, imgW)  # assure imgH >= imgW
-
-        transform = resizeNormalize((imgW, imgH))
-        images = [transform(image) for image in images]
-        images = torch.cat([t.unsqueeze(0) for t in images], 0)
-
-        return images, labels
+def my_collate_fn(batch):
+  images = list()
+  texts = list()
+  for b in batch:
+    images.append(b[0])
+    texts.append(b[1])
+  imgs = torch.cat([t.unsqueeze(0) for t in images], 0) 
+  return imgs, texts
 
--- task2/model.py
+++ task2/model.py
@@ -1,31 +1,34 @@
 from torch.nn import functional as F
 import torch.nn as nn
 
+
 class my_model(nn.Module):
     def __init__(self, num_chars):
         super(my_model, self).__init__()
         self.conv_1 = nn.Conv2d(3, 128, kernel_size=(3, 6), padding=(1, 1))
-        self.pool_1 = nn.MaxPool2d(kernel_size=(2, 2))
         self.conv_2 = nn.Conv2d(128, 64, kernel_size=(3, 6), padding=(1, 1))
+        self.conv_3 = nn.Conv2d(64,64,kernel_size=(3, 6), padding=(1, 1))
         self.pool_2 = nn.MaxPool2d(kernel_size=(2, 2))
-        self.linear_1 = nn.Linear(1152, 64)
+        self.linear_1 = nn.Linear(2048, 64)
         self.drop_1 = nn.Dropout(0.2)
         self.lstm = nn.GRU(64, 32, bidirectional=True, num_layers=2, dropout=0.25, batch_first=True)
         self.output = nn.Linear(64, num_chars)
 
 
-    def forward(self, images):
-        bs, _, _, _ = images.size() 
+    def forward(self, images):    
+        bs, _, _, _ = images.size()   
         x = F.relu(self.conv_1(images)) 
-        x = self.pool_1(x)
         x = F.relu(self.conv_2(x))
-        x = self.pool_2(x) 
+        #x = self.pool_2(x)
+        x = F.relu(self.conv_3(x))
         x = x.permute(0, 3, 1, 2)
+        print(x.size(1))
         x = x.view(bs, x.size(1), -1)
+        print(x.shape)
         x = F.relu(self.linear_1(x))
         x = self.drop_1(x)
         x, _ = self.lstm(x)
         x = self.output(x)
-        x = x.permute(1, 0, 2)
+        x = x.permute(1, 0, 2)# 72x8x19
         
         return x
